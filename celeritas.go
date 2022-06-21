251a264a80e1c6ff03de429cef81c7da80742383
Francisco A. Garcia <frangarcia38@hotmail.com>
2022-06-21 07:04:50 -0600

    Commit con los siguientes cambios: - Creacion de funciones helpers para la creacion de directiorios y   archivos. - Configuraciones iniciales del paquete

---
+++ celeritas.go
@@ -0,0 +1,103 @@
+package celeritas
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+const version = "1.0.0"
+
+type Celeritas struct {
+	AppName   string
+	Debug     bool
+	Version   string
+	ErrorLog  *log.Logger
+	InfoLog   *log.Logger
+	RoothPath string
+	config    config
+}
+
+type config struct {
+	port     string
+	renderer string
+}
+
+func (c *Celeritas) New(rootPath string) error {
+	pathConfig := initPaths{
+		rootPath:    rootPath,
+		folderNames: []string{"handlers", "migrations", "views", "data", "public", "tmp", "logs", "middleware"},
+	}
+
+	err := c.Init(pathConfig)
+
+	if err != nil {
+		return err
+	}
+
+	err = c.checkDotEnv(rootPath)
+
+	if err != nil {
+		return err
+	}
+
+	//leemos el .env
+	err = godotenv.Load(rootPath + "/.env")
+	if err != nil {
+		return err
+	}
+
+	//creamos logs
+	infoLog, errorLog := c.startLoggers()
+	c.InfoLog = infoLog
+	c.ErrorLog = errorLog
+
+	c.Debug, _ = strconv.ParseBool(os.Getenv("DEBUG"))
+	c.Version = version
+	c.RoothPath = rootPath
+
+	c.config = config{
+		port:     os.Getenv("PORT"),
+		renderer: os.Getenv("RENDERER"),
+	}
+
+	return nil
+}
+
+func (c *Celeritas) Init(p initPaths) error {
+	root := p.rootPath
+
+	for _, path := range p.folderNames {
+		//creamos folder si no existe
+		err := c.CreateDirIfNotExist(root + "/" + path)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (c *Celeritas) checkDotEnv(path string) error {
+	err := c.CreateFileIfNotExists(fmt.Sprintf("%s/.env", path))
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Celeritas) startLoggers() (*log.Logger, *log.Logger) {
+	var infoLog *log.Logger
+	var errorLog *log.Logger
+
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	infoLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	return infoLog, errorLog
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/frangar97/celeritas
+
+go 1.18
+
+require github.com/joho/godotenv v1.4.0
