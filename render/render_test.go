21e3f7319f3986b32040d609637694fdd7b7209e
Francisco A. Garcia <frangarcia38@hotmail.com>
2022-06-27 16:44:07 -0600

    Test iniciales para paquete render

---
+++ render/render_test.go
@@ -0,0 +1,33 @@
+package render
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestRender_Page(t *testing.T) {
+	r, err := http.NewRequest("GET", "/some-url", nil)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	w := httptest.NewRecorder()
+
+	testRenderer.Renderer = "go"
+	testRenderer.RoothPath = "./testdata"
+
+	err = testRenderer.Page(w, r, "home", nil, nil)
+
+	if err != nil {
+		t.Error("Error renderizando la pagina", err)
+	}
+
+	testRenderer.Renderer = "jet"
+	err = testRenderer.Page(w, r, "home", nil, nil)
+
+	if err != nil {
+		t.Error("Error renderizando la pagina", err)
+	}
+}
