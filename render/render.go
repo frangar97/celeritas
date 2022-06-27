f6096e604c6dab42a67b4f8dd2bf0b745badeb1d
Francisco A. Garcia <frangarcia38@hotmail.com>
2022-06-27 17:03:15 -0600

    Test por tipo de renderizador

--- render/render.go
+++ render/render.go
@@ -1,6 +1,7 @@
 package render
 
 import (
+	"errors"
 	"fmt"
 	"html/template"
 	"log"
@@ -36,9 +37,10 @@ func (c *Render) Page(w http.ResponseWriter, r *http.Request, view string, varia
 		return c.GoPage(w, r, view, data)
 	case "jet":
 		return c.JetPage(w, r, view, variables, data)
+	default:
 	}
 
-	return nil
+	return errors.New("No se ha especificado renderizador")
 }
 
 func (c *Render) GoPage(w http.ResponseWriter, r *http.Request, view string, data interface{}) error {
--- render/render_test.go
+++ render/render_test.go
@@ -19,14 +19,63 @@ func TestRender_Page(t *testing.T) {
 	testRenderer.RoothPath = "./testdata"
 
 	err = testRenderer.Page(w, r, "home", nil, nil)
-
 	if err != nil {
 		t.Error("Error renderizando la pagina", err)
 	}
 
+	err = testRenderer.Page(w, r, "no-file", nil, nil)
+	if err == nil {
+		t.Error("Error renderizando pagina no existente", err)
+	}
+
 	testRenderer.Renderer = "jet"
 	err = testRenderer.Page(w, r, "home", nil, nil)
+	if err != nil {
+		t.Error("Error renderizando la pagina", err)
+	}
 
+	err = testRenderer.Page(w, r, "no-file", nil, nil)
+	if err == nil {
+		t.Error("Error renderizando pagina no existente", err)
+	}
+
+	testRenderer.Renderer = ""
+	err = testRenderer.Page(w, r, "home", nil, nil)
+	if err == nil {
+		t.Error("No se retorno error sin especificar renderizador", err)
+	}
+
+}
+
+func TestRender_GoPage(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest("GET", "/url", nil)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	testRenderer.Renderer = "go"
+	testRenderer.RoothPath = "./testdata"
+
+	err = testRenderer.Page(w, r, "home", nil, nil)
+	if err != nil {
+		t.Error("Error renderizando la pagina", err)
+	}
+}
+
+func TestRender_JetPage(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest("GET", "/url", nil)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	testRenderer.Renderer = "jet"
+	testRenderer.RoothPath = "./testdata"
+
+	err = testRenderer.Page(w, r, "home", nil, nil)
 	if err != nil {
 		t.Error("Error renderizando la pagina", err)
 	}
