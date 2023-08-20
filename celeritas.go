407212ab7e6c8a33b35cea83dd62f47dc68e6ad9
Francisco <frangarcia38@hotmail.com>
2023-08-20 08:59:27 -0600

    feat: logs for info and errors

--- celeritas.go
+++ celeritas.go
@@ -2,6 +2,9 @@ package celeritas
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 )
@@ -9,9 +12,12 @@ import (
 const version = "1.0.0"
 
 type Celeritas struct {
-	AppName string
-	Debug   bool
-	Version string
+	AppName  string
+	Debug    bool
+	Version  string
+	ErrorLog *log.Logger
+	InfoLog  *log.Logger
+	RootPath string
 }
 
 func (c *Celeritas) New(rootPath string) error {
@@ -35,6 +41,13 @@ func (c *Celeritas) New(rootPath string) error {
 		return err
 	}
 
+	// create loggers
+	infoLog, errorLog := c.startLoggers()
+	c.InfoLog = infoLog
+	c.ErrorLog = errorLog
+	c.Debug, _ = strconv.ParseBool(os.Getenv("DEBUG"))
+	c.Version = version
+
 	return err
 }
 
@@ -57,4 +70,14 @@ func (c *Celeritas) checkDotEnv(path string) error {
 	}
 
 	return err
+}
+
+func (c *Celeritas) startLoggers() (*log.Logger, *log.Logger) {
+	var infoLog *log.Logger
+	var errorLog *log.Logger
+
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	return infoLog, errorLog
 }
