831d5ff82af2aa008b9b4dcffe07af5588fadf16
Francisco <frangarcia38@hotmail.com>
2023-09-03 16:21:40 -0600

    feat: helper to generate random string

--- helpers.go
+++ helpers.go
@@ -1,6 +1,25 @@
 package celeritas
 
-import "os"
+import (
+	"crypto/rand"
+	"os"
+)
+
+const (
+	randomString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0987654321_+"
+)
+
+func (c *Celeritas) RandomString(n int) string {
+	s, r := make([]rune, n), []rune(randomString)
+
+	for i := range s {
+		p, _ := rand.Prime(rand.Reader, len(r))
+		x, y := p.Uint64(), uint64(len(r))
+		s[i] = r[x%y]
+	}
+
+	return string(s)
+}
 
 func (c *Celeritas) CreateDirIfNotExist(path string) error {
 	if _, err := os.Stat(path); os.IsNotExist(err) {
