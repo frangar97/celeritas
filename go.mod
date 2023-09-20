3985f19771c91b6246094a89d8b3162220e3557d
Francisco <frangarcia38@hotmail.com>
2023-09-19 20:30:07 -0600

    feat: Adding migration package and functions

--- go.mod
+++ go.mod
@@ -8,6 +8,10 @@ require (
 	github.com/alexedwards/scs/v2 v2.5.1 // indirect
 	github.com/fatih/color v1.15.0 // indirect
 	github.com/go-chi/chi/v5 v5.0.10 // indirect
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/golang-migrate/migrate/v4 v4.16.2 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.14.1 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -16,8 +20,10 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.4.3 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.10.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
 	golang.org/x/crypto v0.12.0 // indirect
 	golang.org/x/sys v0.11.0 // indirect
 	golang.org/x/text v0.12.0 // indirect
---
+++ migrations.go
@@ -0,0 +1,65 @@
+package celeritas
+
+import "github.com/golang-migrate/migrate/v4"
+
+func (c *Celeritas) MigrateUp(dsn string) error {
+	m, err := migrate.New("file://"+c.RootPath+"/migrations", dsn)
+	if err != nil {
+		return err
+	}
+
+	defer m.Close()
+
+	if err = m.Up(); err != nil {
+		c.ErrorLog.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+func (c *Celeritas) MigrateDownAll(dsn string) error {
+	m, err := migrate.New("file://"+c.RootPath+"/migrations", dsn)
+	if err != nil {
+		return err
+	}
+
+	defer m.Close()
+
+	if err = m.Down(); err != nil {
+		c.ErrorLog.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+func (c *Celeritas) Steps(n int, dsn string) error {
+	m, err := migrate.New("file://"+c.RootPath+"/migrations", dsn)
+	if err != nil {
+		return err
+	}
+
+	defer m.Close()
+
+	if err = m.Steps(n); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Celeritas) MigrateForce(dsn string) error {
+	m, err := migrate.New("file://"+c.RootPath+"/migrations", dsn)
+	if err != nil {
+		return err
+	}
+
+	defer m.Close()
+
+	if err = m.Force(-1); err != nil {
+		return err
+	}
+
+	return nil
+}
