5f77a0c645817aa31c17e09b770a972bd2c388f8
Francisco <frangarcia38@hotmail.com>
2023-09-19 20:51:00 -0600

    feat: templates for cli

---
+++ cmd/cli/make.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+func doMake(arg2, arg3 string) error {
+
+	switch arg2 {
+	case "migration":
+		dbType := cel.DB.DatabaseType
+
+		if arg3 == "" {
+			exitGracefully(errors.New("you must give the migration name"))
+		}
+
+		fileName := fmt.Sprintf("%d_%s", time.Now().UnixMicro(), arg3)
+
+		upFile := cel.RootPath + "/migrations" + fileName + "." + dbType + ".up.sql"
+		downFile := cel.RootPath + "/migrations" + fileName + "." + dbType + ".down.sql"
+
+		err := copyFileFromTemplate("templates/migrations/migration."+dbType+".up.sql", upFile)
+		if err != nil {
+			exitGracefully(err)
+		}
+
+		err = copyFileFromTemplate("templates/migrations/migration."+dbType+".down.sql", downFile)
+		if err != nil {
+			exitGracefully(err)
+		}
+	}
+
+	return nil
+}
